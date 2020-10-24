9258702c1a2998135a183b3eb512c4372609a5fe
sanketpr@buffalo.edu <sanketpr@buffalo.edu>
2020-10-23 20:24:59 -0400

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ProfilePage';
+  firstName = "Sterling"
+  lastName = "Lockwood"
+
+  phone = "716-555-6789"
+
+  email = "sterling@strayos.com"
+
+  address = "Building #1, Buffalo, New York - 14203"
+
+  bio = "I am the very model of a modern Major-General, I've information vegetable, animal, and mineral, I know the kings of England, and I quote the fights historical, From Marathon to Waterloo, in order categorical; I'm very well acquainted too with matters mathematical, I understand equations, both the simple and quadratical, About binomial theorem I'm teeming with a lot o' news--- With many cheerful facts about the square of the hypotenuse."
 }
